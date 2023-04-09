af8f0564284de2641c2326e763a301c1954d5faf
Manny <125616644+mannygitofficial@users.noreply.github.com>
2023-04-09 17:58:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+  navbarToggle.classList.toggle('active');
+  navbarMenu.classList.toggle('active');
+});
